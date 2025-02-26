e6c97d3d4dcda3a939d1d580d241b4c340b08686
martin <martin@stepfun.com>
2025-02-26 19:10:55 +0800

    delete

--- build-vllm.sh
+++
@@ -1,6 +0,0 @@
-eval $(curl -s deploy.i.shaipower.com/httpproxy)
-
-docker build -f Dockerfile-vllm -t test-audio:12.4.1 . \
-        --build-arg http_proxy="$http_proxy" \
-        --build-arg https_proxy="$https_proxy" \
-        --build-arg no_proxy="$no_proxy"
